ef2ce16a47e42c4dfac567e2ffbc72199b01c966
Isaac Jairi da Silva <60949791+Isaac-jairi@users.noreply.github.com>
2020-03-04 18:17:55 -0300

    Add files via upload

---
+++ LabelTest/src/labeltest/LabelFrame.java
@@ -0,0 +1,39 @@
+package labeltest;
+
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class LabelFrame extends JFrame {
+
+    private JLabel label1;
+    private JLabel label2;
+    private JLabel label3;
+
+    public LabelFrame() {
+
+        super("Testando rotulo");
+        setLayout(new FlowLayout());
+
+        label1 = new JLabel("Rotulo com Texto");
+        label1.setToolTipText("Este é o rótulo1");
+        add(label1);
+
+        Icon bug = new ImageIcon(getClass().getResource("bug1.png"));
+        label2 = new JLabel("Rotulo com texto e icone", bug, SwingConstants.LEFT);
+        label2.setToolTipText("Esse é o rotulo2");
+        add(label2);
+
+        label3 = new JLabel();
+        label3.setText("Rotulo com icone e texto na parte inferior");
+        label3.setIcon(bug);
+        label3.setHorizontalTextPosition(SwingConstants.CENTER);
+        label3.setVerticalTextPosition(SwingConstants.BOTTOM);
+        label3.setToolTipText("Este é o rotulo3");
+        add(label3);
+
+    }
+}
